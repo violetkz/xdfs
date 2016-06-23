3eed76450b6c7fa0b043cc2dfc5451dc1ee2ab92
Ken Chow <violet.kz@gmail.com>
2016-06-23 21:49:54 +0800

    add code

--- common/xd_server_base.c
+++ common/xd_server_base.c
@@ -1,8 +1,16 @@
 
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#if 0
 #include <event2/event.h>
 #include <event2/buffer.h>
 #include <event2/http.h>
-
+#endif
 
 
 /* 
@@ -15,11 +23,13 @@
  * 7. exit
  */
 
+#if 0
 int  create_listen_socket(char *bind_addr, int port);
 
 int  create_listen_socket(char *bind_addr, int port) {
 
 }
+#endif
 
 
 typedef struct {
@@ -35,6 +45,7 @@ worker_processes_ctx *worker_processes_ctx_new(int max_worker_num) {
     
     if (new_ctx) {
         memset(new_ctx, 0x00, sizeof(worker_processes_ctx));
+        new_ctx->max_worker_num = max_worker_num;
         if (max_worker_num > 0) {
             new_ctx->worker_pid_list = malloc(sizeof(pid_t) * max_worker_num);
         }
@@ -51,7 +62,7 @@ void worker_processes_ctx_free(worker_processes_ctx *ctx) {
 }
 
 void worker_action_setup(worker_processes_ctx *ctx, 
-                         int (*action)(void *param) func, 
+                         int (*func)(void *), 
                          void *param) {
     if (ctx) {
         ctx->action = func;
@@ -63,6 +74,7 @@ void worker_action_setup(worker_processes_ctx *ctx,
 int create_worker_proecess(worker_processes_ctx *ctx) {
     
     int rc = 0;
+    int is_in_child_process = 0;
 
     if (ctx) {
         int i =0;
@@ -72,12 +84,20 @@ int create_worker_proecess(worker_processes_ctx *ctx) {
                 ctx->worker_pid_list[i] = p;
             }
             else if (p == 0) {
+                is_in_child_process = 1;
                 break;  //break out `for` loop
-                ctx->action(ctx->action_param);
             }
             else {
                 rc = -1;            
+                perror("fork failed");
             }
+        }
+
+        if (is_in_child_process) {
+            ctx->action(ctx->action_param);
+        }
+        else {
+            printf("is in master\n");
         }
     }
     
@@ -86,6 +106,7 @@ int create_worker_proecess(worker_processes_ctx *ctx) {
 
 int worker_func(void *param) {
     printf("Nothing to doing. ==\n");
+    return 0;
 }
 
 int main(int argv, char **args){
@@ -95,6 +116,7 @@ int main(int argv, char **args){
     worker_processes_ctx *wp = worker_processes_ctx_new(10);
     worker_action_setup(wp, worker_func, NULL);
     
-    create_worker_proecess(wp)
-    
+    create_worker_proecess(wp);
+
+    wait(NULL); 
 }
