1c272a531e70be069aa625bcfa6a5064ffdec49d
ken <proxy.vk@gmail.com>
2016-06-28 17:40:35 +0800

    update code

--- common/xd_locker.c
+++ common/xd_locker.c
@@ -26,14 +26,14 @@ int xd_shm_init(xd_shm_lock_t *lock) {
     pthread_mutexattr_t mutex_attr;
     pthread_mutexattr_setpshared(&mutex_attr, PTHREAD_PROCESS_SHARED);
 
-    int rc = pthread_mutex_init(lock->mutex_ptr, &mutex_attr) != 0;
-    if (!rc) {
+    int rc = pthread_mutex_init(lock->mutex_ptr, &mutex_attr);
+    if (rc != 0) {
 #define err_str_buf_len  256
         char err_str_buf[err_str_buf_len]; 
         //fixme: errno ? rc?
-        xd_err("pthread_mutex_init failed: %s", 
-               strerror_r(rc, err_str_buf, err_str_buf_len));
-                return XD_RET_ERR;
+        strerror_r(rc, err_str_buf, err_str_buf_len);
+        xd_err("pthread_mutex_init failed: %s",  err_str_buf);
+        return XD_RET_ERR;
     }
 
     return XD_RET_OK; 
@@ -43,9 +43,10 @@ int xd_shm_lock(xd_shm_lock_t *lock) {
    
     int ret = XD_RET_ERR;
     if (lock && lock->mutex_ptr) {
+        xd_debug("call lock");
         ret = pthread_mutex_lock(lock->mutex_ptr);
     }
-    
+    xd_debug("%s: %d", __func__, ret);
     return ret;
 }
 
--- common/xd_server_base.c
+++ common/xd_server_base.c
@@ -5,16 +5,46 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <errno.h>
 
 #include "xd_log.h"
 #include "xd_worker_pool.h"
+#include "xd_locker.h"
 
-
+typedef struct {
+    int *count;
+    xd_shm_lock_t lock;
+} count_t;
 int worker_func(void *param) {
-    printf("child: Nothing to doing. ==\n");
+    
+    count_t *c =  param;
+    
+    xd_shm_lock(&(c->lock));
+    *c->count +=1;
+    printf("%d\n", *(c->count));
     //sleep(1);
+    xd_shm_unlock(&(c->lock));
     return 0;
 }
+
+int * test(){
+
+    int fd = open("/dev/zero", O_RDWR, 0);
+    if (fd == -1) {
+        xd_err("open /dev/zero failed");
+        return NULL;
+    }
+
+    int *count = mmap(0, sizeof(int), 
+                           PROT_READ|PROT_WRITE, 
+                           MAP_SHARED, fd, 0);
+
+    close(fd);
+    return count;
+}
+
 
 int main(int argv, char **args){
 
@@ -23,7 +53,13 @@ int main(int argv, char **args){
     if (!wp) { 
         exit(1);
     }
-    worker_pool_ctx_set_action(wp, worker_func, NULL);
+
+    count_t  c;
+    c.count = test();
+    *(c.count) = 0;
+    xd_shm_init(&c.lock);
+    
+    worker_pool_ctx_set_action(wp, worker_func, &c);
 
     worker_pool_start(wp);
 
@@ -32,5 +68,6 @@ int main(int argv, char **args){
     worker_pool_ctx_free(wp);
     xd_debug("master process exit");
 
+    xd_shm_destory(&c.lock);
     return 0;
 }
