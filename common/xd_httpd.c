4a2d820db705bf8a14af0640b15a79a854e06bbc
ken <proxy.vk@gmail.com>
2016-07-12 00:54:52 +0800

    placeholder

---
+++ common/xd_httpd.c
@@ -0,0 +1,26 @@
+
+#include <sys/socket.h>
+#include <event2/buffer.h>
+#include <event2/event.h>
+#include <event2/http.h>
+#include <event2/keyvalq_struct.h>
+#include <event2/util.h>
+
+
+#ifdef WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <windows.h>
+#include <io.h>
+#include <fcntl.h>
+#ifndef S_ISDIR
+#define S_ISDIR(x) (((x) & S_IFMT) == S_IFDIR)
+#endif
+#else
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <dirent.h>
+#endif
---
+++ common/xd_httpd.h
@@ -0,0 +1,5 @@
+
+
+
+int xd_httpd_init(const char *addr, unsigned int port);
+int xd_httpd_
